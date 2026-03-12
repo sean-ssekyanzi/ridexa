5aa328bd788e45e9b5ecf36804e3590638dcce5e
sean-ssekyanzi <msean481@gmail.com>
2026-03-12 11:50:03 +0300

    remove app.run

--- main.py
+++ main.py
@@ -92,8 +92,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 port = int(os.environ.get("PORT",8000))
-app.run(host="0.0.0.0",
-        port=port)
 
 # class TransactionBase(BaseModel):
 #     amount: float
